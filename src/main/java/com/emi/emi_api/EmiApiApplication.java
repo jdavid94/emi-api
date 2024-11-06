8718e34b35d98338b8b97b33cd66fcc4272d8e47
jdavi <jdavid94@hotmail.com>
2024-11-06 12:25:54 +0100

    first commit

---
+++ src/main/java/com/emi/emi_api/EmiApiApplication.java
@@ -0,0 +1,13 @@
+package com.emi.emi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emi/emi_api/EmiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emi.emi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
